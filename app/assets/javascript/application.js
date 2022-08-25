2148770d02adfd7d4cb605fbd5bcdb7959849a0f
Alex Trigolos <alexvlg34@gmail.com>
2022-08-25 23:54:31 +0300

    reverse paginate with a choice of page

--- app/assets/javascript/application.js
+++
@@ -1,3 +0,0 @@
-function ask(value){
-    return confirm(value)
-}
---
+++ lib/remote_link_pagination.rb
@@ -0,0 +1,12 @@
+module RemoteLinkPagination
+  class LinkRenderer < BootstrapPagination::Rails
+    def link(text, target, attributes = {})
+      attributes['data-remote'] = true
+      super
+    end
+
+    def gap
+      tag :li, tag(:span, '&hellip;&hearts;', class: 'gap'), 'data-go-to-page': true
+    end
+  end
+end
