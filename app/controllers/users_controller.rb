86e493a2e1e679c3c3f6eed7f209d298e02ee2cc
Alex Trigolos <alexvlg34@gmail.com>
2022-09-09 01:16:29 +0300

    now we can update password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,36 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  include Authentication
   def index
     @users = User.order(created_at: 'asc').paginate(page: params[:page], per_page: 3)
   end
 
   def new
-    @user = User.new
+    # @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      if @user.authenticate(user_params[:password])
+        authenticated(@user)
+      else
+        flash[:warning] = t('.warning')
+        render 'new'
+      end
     else
       render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.find(session[:user_id])
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
+    if @user&.update(user_params)
       flash[:success] = t('.success')
       redirect_to articles_path
     else
@@ -59,5 +64,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def authenticated(user)
+    session[:user_id] = user.id
+    remember(user) if user_params[:remember_me] == '1'
+    flash[:success] = "Welcome to the alpha blog #{@user.username}"
+    redirect_to users_path(user)
   end
 end
