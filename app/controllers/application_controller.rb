297468907e8acb409d207081de864220d0521f9e
Alex Trigolos <alexvlg34@gmail.com>
2022-09-15 00:00:04 +0300

    user and session to normal form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    user = session[:user_id].present? ? user_from_session : user_from_token
-
-    @current_user ||= user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
@@ -15,30 +13,9 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    return if logged_in?
-
-    # rubocop:disable Rails/I18nLocaleTexts
-    flash[:danger] = 'You must be logged in to perform that action'
-    # rubocop:enable Rails/I18nLocaleTexts
-    redirect_to root_path
-  end
-
-  private
-
-  def user_from_session
-    User.find(session[:user_id])
-  end
-
-  def user_from_token
-    user = User.find_by(id: cookies.encrypted[:user_id])
-    token = cookies.encrypted[:remember_token]
-
-    return unless user&.remember_token_authenticated?(token)
-
-    session[:user_id] = user.id
-    # rubocop:disable Rails/I18nLocaleTexts
-    flash[:success] = 'You have logged in by cookies'
-    # rubocop:enable Rails/I18nLocaleTexts
-    user
+    unless logged_in?
+      flash[:danger] = t('.danger')
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  include Authentication
+
   def index
     @users = User.order(created_at: 'asc').paginate(page: params[:page], per_page: 3)
   end
@@ -14,7 +14,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       if @user.authenticate(user_params[:password])
-        authenticated(@user)
+        session[:user_id] = user.id
+        flash[:success] = t('.success')
+        redirect_to users_path(user)
       else
         flash[:warning] = t('.warning')
         render 'new'
@@ -59,10 +61,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
-  def authenticated(user)
-    session[:user_id] = user.id
-    remember(user) if user_params[:remember_me] == '1'
-    flash[:success] = "Welcome to the alpha blog #{@user.username}"
-    redirect_to users_path(user)
-  end
 end
