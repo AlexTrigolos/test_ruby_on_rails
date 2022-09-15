b4d25e05c0078ca2aa162efe8a55cd864300d7a7
Alex Trigolos <alexvlg34@gmail.com>
2022-09-15 13:30:42 +0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_021000) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_102223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,13 +22,27 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_021000) do
     t.integer "user_id"
   end
 
+  create_table "request_responses", force: :cascade do |t|
+    t.string "remote_ip"
+    t.string "request_method"
+    t.string "request_url"
+    t.integer "response_status"
+    t.text "response_content_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_token_digest"
     t.string "username"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
