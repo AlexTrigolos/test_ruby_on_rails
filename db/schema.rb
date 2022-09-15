dabfa7534a1eaffa726c278610dbbe159379e129
Alex Trigolos <alexvlg34@gmail.com>
2022-09-15 13:43:40 +0300

    before start with devise

--- db/schema.rb
+++ db/schema.rb
@@ -37,12 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_102223) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
