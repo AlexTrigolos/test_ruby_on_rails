8b8ef82583e276192a7a55e9aedda32a5b4480ae
Alex Trigolos <alexvlg34@gmail.com>
2022-09-24 00:49:34 +0300

    mb normal migration

--- db/migrate/20220916174250_add_devise_to_users.rb
+++ db/migrate/20220916174250_add_devise_to_users.rb
@@ -2,10 +2,10 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
-    create_table :users do |t|
+    change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ''
-      t.string :username,           null: false, default: ''
+      # t.string :email,              null: false, default: ''
+      # t.string :username,           null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
